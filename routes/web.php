2035a9a228a8574d292e3d87eaa29165aa64cd1d
Bùi Cao Chinh <=>
2020-10-17 12:07:39 +0700

    slide

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,8 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin','namespace'=>'App\Http\Controllers'], function () {
     Route::resource('theloai', 'TheLoaiController')->except(['show']);
-});
-
-
-Route::group(['prefix' => 'admin','namespace'=>'App\Http\Controllers'], function () {
     Route::resource('slide', 'SlideController')->except(['show']);
 });
+
+
+
