79b90792a33bbd09b37113695b4c6b93b82cf6b0
SpringB'be <48855145+tridung240400@users.noreply.github.com>
2020-10-18 09:27:30 +0700

    đổi chỗ

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -158,28 +158,7 @@
 </div>
 <!-- END section -->
 
-<div class="py-0">
-    <div class="container">
-        <div class="half-post-entry d-block d-lg-flex bg-light">
-            <div class="img-bg" style="background-image: url('pages_asset/images/big_img_1.jpg')"></div>
-            <div class="contents">
-                <span class="caption">Editor's Pick</span>
-                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
-                <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate vero obcaecati
-                    natus adipisci necessitatibus eius, enim vel sit ad reiciendis. Enim praesentium magni delectus
-                    cum, tempore deserunt aliquid quaerat culpa nemo veritatis, iste adipisci excepturi consectetur
-                    doloribus aliquam accusantium beatae?</p>
 
-                <div class="post-meta">
-                    <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">Food</a></span>
-                    <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span
-                            class="icon-star2"></span></span>
-                </div>
-
-            </div>
-        </div>
-    </div>
-</div>
 
 <div class="site-section">
     <div class="container">
@@ -401,6 +380,28 @@
         </div>
     </div>
 </div>
+<div class="py-0">
+    <div class="container">
+        <div class="half-post-entry d-block d-lg-flex bg-light">
+            <div class="img-bg" style="background-image: url('pages_asset/images/big_img_1.jpg')"></div>
+            <div class="contents">
+                <span class="caption">Editor's Pick</span>
+                <h2><a href="blog-single.html">News Needs to Meet Its Audiences Where They Are</a></h2>
+                <p class="mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate vero obcaecati
+                    natus adipisci necessitatibus eius, enim vel sit ad reiciendis. Enim praesentium magni delectus
+                    cum, tempore deserunt aliquid quaerat culpa nemo veritatis, iste adipisci excepturi consectetur
+                    doloribus aliquam accusantium beatae?</p>
+
+                <div class="post-meta">
+                    <span class="d-block"><a href="#">Dave Rogers</a> in <a href="#">Food</a></span>
+                    <span class="date-read">Jun 14 <span class="mx-1">&bullet;</span> 3 min read <span
+                            class="icon-star2"></span></span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="site-section subscribe bg-light">
     <div class="container">
@@ -419,4 +420,5 @@
         </form>
     </div>
 </div>
+
 @endsection
