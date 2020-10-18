f486423a4ea7546589c643f9241df66a50052bab
SpringB'be <48855145+tridung240400@users.noreply.github.com>
2020-10-18 17:14:38 +0700

    sửa footer

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -231,23 +231,6 @@
     </div>
 </div>
 
-<div class="site-section subscribe bg-light space-subcribe">
-    <div class="container">
-        <form action="#" class="row align-items-center">
-            <div class="col-md-5 mr-auto">
-                <h2>Newsletter Subcribe</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis aspernatur ut at quae omnis
-                    pariatur obcaecati possimus nisi ea iste!</p>
-            </div>
-            <div class="col-md-6 ml-auto">
-                <div class="d-flex">
-                    <input type="email" class="form-control" placeholder="Enter your email">
-                    <button type="submit" class="btn btn-secondary"><span class="icon-paper-plane"></span></button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
 
 
 
