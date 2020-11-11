6acec096b298601261bb2cb66e4b61252d93bc8c
SpringB'be <48855145+tridung240400@users.noreply.github.com>
2020-11-12 00:24:45 +0700

    auto load data and analysis

--- app/Http/Controllers/AutoGenController.php
+++ app/Http/Controllers/AutoGenController.php
@@ -4,25 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\{TinTuc,LoaiTin,TheLoai};
 use Illuminate\Http\Request;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Cache;
 
 class AutoGenController extends Controller
 {
     public function autogen()
     {   
-        for($i=1;$i<=100;$i++){
+        $faker = Faker::create();
+        for($i=1;$i<=10000;$i++){
             $tintuc = new TinTuc();
-            $tintuc->TieuDe = substr(file_get_contents('https://loripsum.net/api/1/short/plaintext'),0,65);
+            $tintuc->TieuDe = $faker->realText(100,2);
             $tintuc->TieuDeKhongDau = changeTitle($tintuc->TieuDe);
             $tintuc->idLoaiTin = rand(1,37);
-            $tintuc->TomTat = substr(file_get_contents('https://loripsum.net/api/1/short/plaintext'),0,150);
-            $tintuc->NoiDung = file_get_contents('https://loripsum.net/api/2/long/plaintext').
+            $tintuc->TomTat = $faker->realText(140,2);
+            $tintuc->NoiDung = $faker->realText(2000,2).
             "<br/><br/><img src='upload/tintuc/bo-hinh-nen-chat-luong-cao-".rand(1,101).".jpg' alt='IMG ERROR!' style='max-width: 100%;
             height: auto;'><br/><br/>".
-            file_get_contents('https://loripsum.net/api/2/long/plaintext');
+            $faker->realText(2000,2);
             $tintuc->NoiBat = rand(0,1);
             $tintuc->SoLuotXem = rand(100,1000);
             $tintuc->Hinh="bo-hinh-nen-chat-luong-cao-".rand(1,101).".jpg"; 
             $tintuc->save();
             echo ('Thêm thành công!');}
+        Cache::flush();
     }
+
 }
