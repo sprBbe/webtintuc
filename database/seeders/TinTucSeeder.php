600b63311a0dd79c2ad5a86d4e2c9c1379daad78
tridung240400@gmail.com <0383Springbbe2404>
2020-12-08 22:19:57 +0700

    add seeder

---
+++ database/seeders/TinTucSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\TinTuc;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class TinTucSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        $tieude = $faker->realText(80, 2);
+        $tieudekhongdau = changeTitle($tieude);
+        $tintucs = [
+            [
+                'TieuDe' => $tieude,
+                'TieuDeKhongDau' => $tieudekhongdau,
+                'idLoaiTin' => rand(1, 37),
+                'TomTat' => $faker->realText(125, 2),
+                'NoiDung' => $faker->realText(2000, 2) .
+                    "<br/><br/><img src='upload/tintuc/bo-hinh-nen-chat-luong-cao-" . rand(1, 101) . ".jpg' alt='IMG ERROR!' style='max-width: 100%;
+                height: auto;'><br/><br/>" .
+                    $faker->realText(2000, 2),
+                'NoiBat' => rand(0, 1),
+                'SoLuotXem' => rand(100, 1000),
+                'Hinh' => "bo-hinh-nen-chat-luong-cao-" . rand(1, 101) . ".jpg",
+            ],
+        ];
+        for ($i = 0; $i<10; $i++){
+            foreach ($tintucs as $tintuc) {
+                TinTuc::create($tintuc);
+            }
+        }
+
+    }
+}
